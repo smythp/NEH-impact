0cbe99e58ac3a7cd8865395ea23cdd9469521249
Patrick Smyth <patricksmyth01@gmail.com>
2017-03-30 19:58:13 -0400

    Sorting by distance works! Added institution to template

--- app/views.py
+++ app/views.py
@@ -19,6 +19,12 @@ def db_connect(db_name):
     return conn
 
 
+def question_mark_sequence(num):
+    "Makes sequence of question marks that is NUM long for use in building SQL statements."
+    x = '?, ' * num
+    return x[:-2]
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -53,7 +59,7 @@ def index():
         distance_query = 'SELECT end_zip from distances WHERE start_zip=? AND distance<?;'
 
         zips_to_return = [zipcode[0] for zipcode in cur.execute(distance_query, (zip_input, distance,)).fetchall()]
-        
+
         query = 'SELECT Institution, \
 InstCity, \
 InstState, \
@@ -64,11 +70,14 @@ Program, \
 ProjectDesc, \
 ToSupport, \
 PrimaryDiscipline \
-FROM grants WHERE ShortPostal=? \
-AND (ProjectDesc is not null OR ToSupport is not null);'
+FROM grants WHERE ShortPostal in (%s) \
+AND (ProjectDesc is not null OR ToSupport is not null);' % question_mark_sequence(len(zips_to_return))
+        print(tuple(zips_to_return))
+        print(query)
+
         conn2 = db_connect('grants.db')
         cur = conn2.cursor()
-        grants = cur.execute(query, (zip_input,)).fetchall()
+        grants = cur.execute(query, tuple(zips_to_return)).fetchall()
 
         return render_template('results.html', grants=grants, form=form, jquery=True)
         
