724c9897c7fced28514a6cc1fd494c8372d8c20e
Patrick Smyth <patricksmyth01@gmail.com>
2017-04-11 21:03:08 -0400

    No more resubmitting form over and over Created URL endpoints for results so they don't need a POST request Fixed relative URLs for links on results page

--- app/views.py
+++ app/views.py
@@ -52,25 +52,41 @@ def internal_server_error(e):
 @app.route('/index.html', methods=['GET', 'POST'])
 def index():
     form = ZipForm(request.form)
-    if request.method == 'POST' and form.validate():
 
+    if request.method == 'POST' and form.validate():
         zip_input = request.form['zip']
 
         if 'distance' in request.form:
-            distance = request.form['distance']
+                 distance = request.form['distance']
         else:
-            distance = 10.0
+            distance = 10
 
-        conn = sqlite3.connect(config.DATABASE)
-        cur = conn.cursor()
-        distance_query = 'SELECT end_zip from distances WHERE start_zip=? AND distance<?;'
+        return redirect(url_for('search', zip_input=zip_input, distance=distance))
+
         
-        zips_to_return = [zipcode[0] for zipcode in cur.execute(distance_query, (zip_input, distance,)).fetchall()]
+    return render_template('index.html', form=form,
+                               jquery=True, title="Home")
 
-        # Add the original zip back in to the set of zips to be searched
-        zips_to_return.append(zip_input)
 
-        query = 'SELECT Institution, \
+@app.route('/test', methods=['GET'])
+def test():
+    return 'ham sandwich'
+
+@app.route('/search/zip/<zip_input>/distance/<distance>', methods=['GET'])
+def search(zip_input, distance):
+
+    form = ZipForm(request.form)
+    
+    conn = sqlite3.connect(config.DATABASE)
+    cur = conn.cursor()
+    distance_query = 'SELECT end_zip from distances WHERE start_zip=? AND distance<?;'
+        
+    zips_to_return = [zipcode[0] for zipcode in cur.execute(distance_query, (zip_input, distance,)).fetchall()]
+
+    # Add the original zip back in to the set of zips to be searched
+    zips_to_return.append(zip_input)
+
+    query = 'SELECT Institution, \
 InstCity, \
 id, \
 InstState, \
@@ -85,60 +101,57 @@ PrimaryDiscipline \
 FROM grants WHERE ShortPostal in (%s) \
 AND (ProjectDesc is not null OR ToSupport is not null);' % question_mark_sequence(len(zips_to_return))
 
-        conn2 = db_connect(config.DATABASE)
-        cur = conn2.cursor()
-        grants = cur.execute(query, tuple(zips_to_return)).fetchall()
+    conn2 = db_connect(config.DATABASE)
+    cur = conn2.cursor()
+    grants = cur.execute(query, tuple(zips_to_return)).fetchall()
 
-        results_count = len(grants)
+    results_count = len(grants)
 
-        display_names = {
-            "research_education": "Research and Education",
-            "other_humanities": "Other Humanities",
-            "education": "Education",
-            "research": "Research",
-            "public_programs": "Public Programs",
-            "federal_state": "Federal or State Collaborations",
-            "challenge_grants": "Challenge Grants",
-            "preservation_access": "Preservation and Access",
-            "digital_humanities": "Digital Humanities",
-            }
+    display_names = {
+        "research_education": "Research and Education",
+        "other_humanities": "Other Humanities",
+        "education": "Education",
+        "research": "Research",
+        "public_programs": "Public Programs",
+        "federal_state": "Federal or State Collaborations",
+        "challenge_grants": "Challenge Grants",
+        "preservation_access": "Preservation and Access",
+        "digital_humanities": "Digital Humanities",
+    }
 
-        division_description = {
-            "research_education": "Research into areas related to pedagogy.",
-            "other_humanities": "Humanities projects that don't fit neatly into other categories.",
-            "education": "Grants related to teaching and education.",
-            "research": "Grants related to research in the humanities.",
-            "public_programs": "Projects and programs that engage the public directly.",
-            "federal_state": "Collaborations with state or federal government.",
-            "challenge_grants": "Grants that challenge institutions to raise additional funds that are matched by the NEH at a set rate.",
-            "preservation_access": "Grants for preserving, restoring, or digitizing culture.",
-            "digital_humanities": "Grants related to the digital humanities.",
-            }
+    division_description = {
+        "research_education": "Research into areas related to pedagogy.",
+        "other_humanities": "Humanities projects that don't fit neatly into other categories.",
+        "education": "Grants related to teaching and education.",
+        "research": "Grants related to research in the humanities.",
+        "public_programs": "Projects and programs that engage the public directly.",
+        "federal_state": "Collaborations with state or federal government.",
+        "challenge_grants": "Grants that challenge institutions to raise additional funds that are matched by the NEH at a set rate.",
+        "preservation_access": "Grants for preserving, restoring, or digitizing culture.",
+        "digital_humanities": "Grants related to the digital humanities.",
+        }
 
-        all_divisions = [grant['division_reclassification'] for grant in grants]
+    all_divisions = [grant['division_reclassification'] for grant in grants]
 
-        division_count = Counter(all_divisions)
-        distinct_divisions = set(all_divisions)
+    division_count = Counter(all_divisions)
+    distinct_divisions = set(all_divisions)
 
-        divisions = []
-        for classification in distinct_divisions:
-            divisions.append({
-                "class": classification,
-                "name": display_names[classification],
-                "count": division_count[classification],
-                "description": division_description[classification],
-                })
+    divisions = []
+    for classification in distinct_divisions:
+        divisions.append({
+            "class": classification,
+            "name": display_names[classification],
+            "count": division_count[classification],
+            "description": division_description[classification],
+            })
 
-        return render_template('results.html', grants=grants,
-                               updated_on=config.DATA_UPDATED_ON,
-                               form=form, divisions=divisions, original_zip=zip_input,
-                               results_count=results_count, distance=int(distance),
-                               jquery=True,
-                               title="Results")
+    return render_template('results.html', grants=grants,
+                           updated_on=config.DATA_UPDATED_ON,
+                           form=form, divisions=divisions, original_zip=zip_input,
+                           results_count=results_count, distance=int(distance),
+                           jquery=True,
+                           title="Results")
         
-    else:
-        return render_template('index.html', form=form,
-                               jquery=True, title="Home")
 
     
 @app.route('/grant/<grant_id>', methods=['GET'])
